1bd84c3dcfee047092809086df72470b9c0e82c6
akuyper <a-kuyper@northwestern.edu>
2021-04-14 12:21:16 -0500

    Setup L03

---
+++ README.md
@@ -0,0 +1,4 @@
+## Overview
+
+The main goal of this lab is to improve and expand feature engineering skills. We also want to develop a better sense of how to handle missing data.
+
---
+++ titanic_setup.R
@@ -0,0 +1,15 @@
+# Load package(s)
+library(tidymodels)
+library(tidyverse)
+
+# Seed
+set.seed(3013)
+
+## load data
+titanic <- read_csv("data/titanic.csv") %>%
+  janitor::clean_names() %>%
+  mutate(
+    survived = factor(survived, levels = c("yes", "no")),
+    pclass = factor(pclass)
+  )
+
